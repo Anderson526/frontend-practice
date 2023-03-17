24da34d9f4a35010a2ab441aa16816c4b8927aa0
AndersonC <1megaword1@gmail.com>
2023-03-17 16:59:18 -0500

    update

--- curso practico javascript/clase1/index.js
+++ curso practico javascript/clase1/index.js
@@ -1,3 +1,10 @@
-document.getElementById();
-document.getElementsByClassName();
-document.getElementsByName();
+const valueH1 = document.querySelector('h1'); // etiqueta
+const parrafo = document.querySelector('.parrafo'); // clase
+const input = document.querySelector('#input'); // etiqueta
+
+console.log({
+    valueH1,
+    parrafo,
+    input
+
+});
