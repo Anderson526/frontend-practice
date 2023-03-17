ae543b7256ade6feddabc33aa6961bfe25e17b43
AndersonC <1megaword1@gmail.com>
2023-03-17 17:33:12 -0500

    update

--- curso practico javascript/clase1/index.js
+++ curso practico javascript/clase1/index.js
@@ -8,3 +8,14 @@ console.log({
     input
 
 });
+
+//set and get attibute
+
+parrafo.setAttribute("class","10");
+
+console.log(parrafo.getAttribute("class"));
+
+
+//add and remove class
+valueH1.classList.add("w-100");
+input.classList.remove("name");
